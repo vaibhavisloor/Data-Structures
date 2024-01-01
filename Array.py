5cb3ffaed2553bdd7a64518061810502ec896f64
Vaibhav Isloor <you@example.com>
2024-01-01 08:02:53 +0530

    python-lang

---
+++ Array.py
@@ -0,0 +1,23 @@
+#Adding Element, Deleting Element, Updating Element
+
+list = [1,2,3,4,5,6,7,8,9,10]
+
+
+
+
+# Adding Element
+
+list.insert(3,50) # insert(index,value)
+print(list)
+
+
+#Removing Element
+
+list.remove(3) # .remove(value)
+print(list)
+
+
+#Updating Element
+
+list[2]=3
+print(list)
