9f9524c2e134837ba1f7213c64dacf037a897769
Vaibhav Isloor <viisloor@gmail.com>
2024-08-09 19:48:17 +0530

    prac

--- prac.py
+++ prac.py
@@ -73,64 +73,95 @@
 
 #DoublyLinkedList
 
-class Node:
-    def __init__(self,val) :
-        self.prev = None
-        self.val=val
-        self.next = None
+# class Node:
+#     def __init__(self,val) :
+#         self.prev = None
+#         self.val=val
+#         self.next = None
 
-class DLL:
-    def __init__(self):
-        self.head = None
+# class DLL:
+#     def __init__(self):
+#         self.head = None
 
-    def add_first(self,val):
-        newNode= Node(val)
-        if self.head == None:
-            self.head = newNode
-        else:
-            newNode.next = self.head
-            self.head.prev = newNode
-            self.head = newNode
-    def add_last(self,val):
-        newNode = Node(val)
-        if self.head == None:
-            self.head = newNode
-        else:
-            temp = self.head
-            while temp.next != None:
-                temp = temp.next
-            temp.next = newNode
-            newNode.prev = temp   
+#     def add_first(self,val):
+#         newNode= Node(val)
+#         if self.head == None:
+#             self.head = newNode
+#         else:
+#             newNode.next = self.head
+#             self.head.prev = newNode
+#             self.head = newNode
+#     def add_last(self,val):
+#         newNode = Node(val)
+#         if self.head == None:
+#             self.head = newNode
+#         else:
+#             temp = self.head
+#             while temp.next != None:
+#                 temp = temp.next
+#             temp.next = newNode
+#             newNode.prev = temp   
 
-    def delete_element(self,val):
-        if self.head.val == val:
-            self.head = self.head.next
-        else:
-            temp = self.head
-            while temp.next.val != val:
-                temp = temp.next
-            temp.next = temp.next.next
-            temp.next.next.prev = temp  
+#     def delete_element(self,val):
+#         if self.head.val == val:
+#             self.head = self.head.next
+#         else:
+#             temp = self.head
+#             while temp.next.val != val:
+#                 temp = temp.next
+#             temp.next = temp.next.next
+#             temp.next.next.prev = temp  
 
-    def show_list(self):
-        temp = self.head
-        while temp:
-            print(f"{temp.val} <=> ",end="")
-            temp=temp.next
-        print("None")    
-if __name__ == "__main__":
-    dll=DLL()
+#     def show_list(self):
+#         temp = self.head
+#         while temp:
+#             print(f"{temp.val} <=> ",end="")
+#             temp=temp.next
+#         print("None")    
+# if __name__ == "__main__":
+#     dll=DLL()
 
-    dll.add_first(10)        
-    dll.add_first(12)        
-    dll.add_first(13)        
-    dll.add_last(20)
-    dll.add_last(28)
-    dll.add_last(27)
-    dll.add_last(22)
+#     dll.add_first(10)        
+#     dll.add_first(12)        
+#     dll.add_first(13)        
+#     dll.add_last(20)
+#     dll.add_last(28)
+#     dll.add_last(27)
+#     dll.add_last(22)
 
 
-    dll.delete_element(20)
+#     dll.delete_element(20)
 
-    dll.show_list()
-    
+#     dll.show_list()
+
+
+#Max Heap
+def build_heap(arr,size):
+    i = int(size/2)
+
+    while i >= 0:
+        Heapify(arr,i,size)
+        i-=1
+def Heapify(arr,i,size):
+    left = 2 * i + 1        
+    right = left + 1        
+
+    max = i
+
+    if left < size and arr[left] > arr[max]:
+        max=left
+    if right < size and arr[right] > arr[max]:
+        max=right
+
+    if i!=max:
+
+        arr[i],arr[max] = arr[max],arr[i]
+        Heapify(arr,max,size)
+
+
+arr = [1,2,33,11,23,21,43,21,51]
+
+
+build_heap(arr,len(arr))
+
+print(arr)
