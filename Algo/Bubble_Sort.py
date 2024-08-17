e447cd9c0374529b9cf100b79fd8f2ad3ab74512
Vaibhav Isloor <viisloor@gmail.com>
2024-08-17 12:23:31 +0530

    bubble-sort-recurcion

--- Algo/Bubble_Sort.py
+++ Algo/Bubble_Sort.py
@@ -1,7 +1,21 @@
 arr = [2,5,1,78,34,56,3,9,66]
 print(arr)
-for i in range(len(arr)-1):
-    for j in range(len(arr)-1-i):
-        if arr[j] > arr[j+1]:
-            arr[j],arr[j+1] = arr[j+1],arr[j]
-print(arr)            
+# for i in range(len(arr)-1):
+#     for j in range(len(arr)-1-i):
+#         if arr[j] > arr[j+1]:
+#             arr[j],arr[j+1] = arr[j+1],arr[j]
+# print(arr)            
+
+size=len(arr)
+def bubble(arr,size):
+    if size == 1:
+        return
+    for i in range(size):
+        if i+1 < size:
+            if arr[i] > arr[i+1]:
+                arr[i],arr[i+1] = arr[i+1],arr[i]
+    bubble(arr,size-1)            
+
+bubble(arr,size) 
+
+print(arr)
--- prac.py
+++ prac.py
@@ -1,23 +1,4 @@
-names = [
-    "John.Doe",
-    "jane.doe",
-    "Alice Wonderland",
-    "Mr.Smith",
-    "Dr.Jones",
-    "Emily.",
-    "michael.Clark",
-    "Dr.Jane.Doe",
-    "Ms.Mary-jane",
-    "mr.bean"
-]
-
-valid_name = []
-non_valid_names=[]
-
-for name in names:
-    if all(char not in ["-","_"] for char in name):
-        print(name.title()) 
-
-
-                
-            
+p={
+    'a':1
+}
+print(p['b'])
