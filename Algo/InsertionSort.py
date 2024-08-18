97649a9710d7629fa5270607734d028540d70d24
Vaibhav Isloor <viisloor@gmail.com>
2024-08-18 12:23:31 +0530

    bubble-sort-recurcion

--- Algo/InsertionSort.py
+++ Algo/InsertionSort.py
@@ -1,13 +1,22 @@
 arr = [2,1,6,34,78,456,231,45,62]
 print(arr)
 
+# for i in range(1,len(arr)):
+#     value = arr[i]
+#     index = i
+
+#     while index > 0 and arr[index-1] > value:
+#         arr[index] = arr[index-1]
+#         index-=1
+#     arr[index] = value
+
+# print(arr)
+
 for i in range(1,len(arr)):
     value = arr[i]
     index = i
 
-    while index > 0 and arr[index-1] > value:
-        arr[index] = arr[index-1]
+    while index>0 and arr[index-1] > arr[index]:
+        arr[index],arr[index-1] = arr[index-1],arr[index]
         index-=1
-    arr[index] = value
-
-print(arr)
+print(arr)        
