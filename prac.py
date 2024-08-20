e78154a752817bc19d2cb8107c02a0dc70c8a638
Vaibhav Isloor <viisloor@gmail.com>
2024-08-20 08:45:14 +0530

    max_heap_sort

--- prac.py
+++ prac.py
@@ -1,5 +1,14 @@
 arr = [2,1,6,34,78,456,231,45,62]
 
+def heap_sort(size):
+    build_heap(arr,len(arr))
+    end=len(arr)-1
+    while size > 1:
+        arr[size-1],arr[0] = arr[0],arr[size-1]
+        size-=1
+        heapify(arr,0,size)
+
+
 def build_heap(arr,size):
     index = int(size)//2 - 1
     while index >=0:
@@ -21,5 +30,5 @@ def heapify(arr,index,size):
         arr[max],arr[index] = arr[index],arr[max]
         heapify(arr,max,size)
 
-build_heap(arr,len(arr))        
+heap_sort(len(arr))
 print(arr)
