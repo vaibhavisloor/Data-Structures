9e9d59305cb88812495105e53b678d4bebbd3766
Vaibhav Isloor <viisloor@gmail.com>
2025-01-17 10:49:38 +0530

    CQp1

--- HyperV2/DSA/Circular_Queue.py
+++ HyperV2/DSA/Circular_Queue.py
@@ -5,3 +5,13 @@ class Queue:
         self.rear = -1
         self.size = size
 
+        def enqueue(self, val):
+            if (self.rear + 1) % self.size == self.front:  
+                print("Queue is full")
+            else:
+                self.rear = (self.rear + 1) % self.size  
+                self.array[self.rear] = val
+                print(f"The value {val} has been appended")
+
+                if self.front == -1:
+                    self.front = 0
