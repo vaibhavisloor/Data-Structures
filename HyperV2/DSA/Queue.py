387302cf7329b3ff9130da35c820ecb2c04d5943
Vaibhav Isloor <viisloor@gmail.com>
2025-01-21 11:17:42 +0530

    Qp2

--- HyperV2/DSA/Queue.py
+++ HyperV2/DSA/Queue.py
@@ -5,3 +5,9 @@ class Queue:
         self.front = 0
         self.rear = 0
 
+    def enqueue(self,val):
+        if self.rear < self.size:
+            self.array[self.rear] = val
+            self.rear += 1
+        else:
+            print("Queue is full")
