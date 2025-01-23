7f5bc5bfc6cc87195dae4fe93ef8b85f11b4477b
Vaibhav Isloor <viisloor@gmail.com>
2025-01-23 11:18:24 +0530

    Qp4

--- HyperV2/DSA/Queue.py
+++ HyperV2/DSA/Queue.py
@@ -19,3 +19,17 @@ class Queue:
             print(f"The value {self.array[self.front]} is dequeued")
             self.array[self.front] = None
             self.front += 1
+if __name__ == "__main__":
+    q=Queue(5)
+    q.enqueue(5)
+    q.enqueue(4)
+    q.enqueue(3)
+    q.enqueue(2)
+    q.enqueue(1)
+    q.enqueue(1)
+    q.dequeue()
+    q.dequeue()
+    q.dequeue()
+    q.dequeue()
+    q.dequeue()
+    q.dequeue()
