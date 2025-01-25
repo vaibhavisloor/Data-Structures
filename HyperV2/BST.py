19bcfb37c35308fcf6da2b3b5feacf9dbfeaccdc
Vaibhav Isloor <viisloor@gmail.com>
2025-01-25 12:55:34 +0530

    BSTp

--- HyperV2/BST.py
+++ HyperV2/BST.py
@@ -4,5 +4,12 @@ class Node:
         self.data = data
         self.right = None
     
-
+def insert(root,val):
+    if root is None:
+        return Node(val)
+    elif root.data < val:
+        root.right = insert(root.right,val)
+    elif root.data > val:
+        root.left = insert(root.left,val)
+    return root
 
