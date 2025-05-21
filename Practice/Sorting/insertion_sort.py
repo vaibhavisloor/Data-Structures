aa987ff34d363ca77a07293eda299d8a6bb28840
Vaibhav Isloor <viisloor@gmail.com>
2025-05-21 06:42:23 +0530

    insertion__sort_reimplemented

--- Practice/Sorting/insertion_sort.py
+++ Practice/Sorting/insertion_sort.py
@@ -5,10 +5,18 @@ arr = [random.randint(1, 100) for _ in range(10)]
 print("Random array:", arr)
 
 
+# for i in range(1,len(arr)):
+#     j=i
+#     while j-1 >= 0  and arr[j] < arr[j-1]:
+#         arr[j],arr[j-1] = arr[j-1],arr[j]
+#         j-=1
+
+# print(arr)
+
+
 for i in range(1,len(arr)):
     j=i
-    while j-1 >= 0  and arr[j] < arr[j-1]:
+    while j-1 >= 0 and arr[j-1] > arr[j]:
         arr[j],arr[j-1] = arr[j-1],arr[j]
         j-=1
-
 print(arr)
