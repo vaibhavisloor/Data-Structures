9408e4aacc4f3c3c812a39d3b7f733f524dfd9a9
Vaibhav Isloor <viisloor@gmail.com>
2025-06-06 10:24:42 +0530

    QuickSOrt Implemented

--- Practice/Sorting/quick_sort.py
+++ Practice/Sorting/quick_sort.py
@@ -3,33 +3,58 @@ import random
 array = [random.randint(0, 100) for _ in range(10)]
 print("Original array:", array)
 
-def quick_sort(array,start,end):
-    if start >= end:
-        return
+# def quick_sort(array,start,end):
+#     if start >= end:
+#         return
 
+#     pivot = end
+#     left = start
+#     right = pivot - 1
+
+#     while left < right:
+#         while left < len(array) and array[left] <= array[pivot]:
+#             left+=1
+
+#         while right < len(array) and array[right] >= array[pivot]:
+#             right-=1
+
+#         if left < right:
+#             array[left],array[right] = array[right],array[left]
+
+#     array[pivot],array[left] = array[left],array[pivot]
+
+#     quick_sort(array, start, left - 1)
+#     quick_sort(array, left + 1, end)
+
+# quick_sort(array,0,len(array)-1)
+
+# print(array)
+
+
+def quick_sort(arr,start,end):
+    if start >= end:
+        return 
+    
     pivot = end
     left = start
     right = pivot - 1
 
-    while left < right:
-        while left < len(array) and array[left] <= array[pivot]:
+    while left <= right:
+        while left < len(arr) and arr[left] < arr[pivot]:
             left+=1
-
-        while right < len(array) and array[right] >= array[pivot]:
+        
+        while right >= 0 and arr[right] > arr[pivot]:
             right-=1
 
         if left < right:
-            array[left],array[right] = array[right],array[left]
+            arr[left],arr[right] = arr[right],arr[left]
+    
+    arr[pivot],arr[left] = arr[left],arr[pivot]
 
-    array[pivot],array[left] = array[left],array[pivot]
-
-    quick_sort(array, start, left - 1)
-    quick_sort(array, left + 1, end)
+    quick_sort(arr,start,left-1)
+    quick_sort(arr,left+1,end)
 
 quick_sort(array,0,len(array)-1)
-
 print(array)
-
-
 
     
