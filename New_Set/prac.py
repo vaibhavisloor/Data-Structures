1d823a04889349ff350da1d77b845466bdac411d
Vaibhav Isloor <viisloor@gmail.com>
2026-08-13 19:00:39 +0530

    tiny-rev

---
+++ New_Set/prac.py
@@ -0,0 +1,16 @@
+# a = ['a','n','c','d']
+# print(sorted(a))
+
+# random_dict = {
+#     'z':16,
+#     'x':5,
+#     'g':8
+# }
+
+# print(sorted(random_dict.items(), key= lambda x: x[1]))
+
+
+x = ('Namaste',('Hello'))
+print(list(x))
+
+
